8b83ac3760a53794a91e02e6a699fda4f43bb889
William ONeal <william_oneal@apple.com>
2022-03-10 16:02:54 -0800

    ShopPage, CollectionPreview, CollectionItem updated.

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
+import CollectionItem from '../collection-item/collection-item.component';
+
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
-
     <div className='collection-preview'>
         <h2 className='title'>{title.toUpperCase()}</h2>
         <div className='preview'>
-        {
-            DataTransferItemList.map(item => (
-                <div key={item.id}>{item.name}</div>
-            ))
-        }
+        {items
+            .filter((item, idx) => idx < 4)
+            .map(({id, ...itemProps}) => (
+                <CollectionItem key={id} {...itemProps} />
+        ))}
         </div>
     </div>
-
 )
 
 export default CollectionPreview;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import SHOP_DATA from './shop.data';
-
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
+import './shop.styles.scss';
 class ShopPage extends React.Component {
     constructor(props) {
         super(props);
