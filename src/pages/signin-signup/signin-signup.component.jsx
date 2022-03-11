bf191f30cc0078624d565a9cd296805252240fd7
William ONeal <william_oneal@apple.com>
2022-03-10 17:10:25 -0800

    Update SignInSignUp page.

---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './signin-signup.styles.scss';
+
+const SignInSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+)
+
+export default SignInSignUpPage;
